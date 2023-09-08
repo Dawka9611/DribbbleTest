381adc7d8e65044532c97fc90a9f7da3768027fd
dawka <udawaadulam9611@gmail.com>
2023-09-08 09:39:33 +0800

    last commit

--- src/pages/EmptyPageGym.jsx
+++ src/pages/EmptyPageGym.jsx
@@ -2,7 +2,7 @@ import LayoutComponent from "../components/Layout";
 
 const EmptyPage = () => {
     return <LayoutComponent>
-        <div className="textBig text-6xl flex items-center justify-center h-96 text-white font-bold tracking-wide h-full">
+        <div className="textBig text-6xl flex items-center justify-center text-white font-bold tracking-wide h-full p-48">
             No content to show
         </div>
     </LayoutComponent>
